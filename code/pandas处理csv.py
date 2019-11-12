0923cecfe031587865fba3e12fc6576fe2a14ee6
kate711 <kate711@aliyun.com>
2019-11-12 20:22:24 +0800

    1911122022

--- code/pandas处理csv.py
+++ code/pandas处理csv.py
@@ -1,3 +1,4 @@
+# 1 读写
 import sys
 import pandas as pd
 
@@ -6,3 +7,30 @@ output_file = 'C:/Users/Administrator/Desktop/Book3.csv'
 data_frame = pd.read_csv(input_file)
 print(data_frame)
 data_frame.to_csv(output_file, index=False)
+
+# 2 筛选特定的行
+
+import sys
+import pandas as pd
+
+input_file = 'C:/Users/Administrator/Desktop/Book1.csv'
+output_file = 'C:/Users/Administrator/Desktop/Book6.csv'
+data_frame = pd.read_csv(input_file)
+data_frame['Cost'] = data_frame['Cost'].str.strip('$').astype(float)
+
+data_frame_value_meets_condition = data_frame.loc[
+                                   (data_frame['Supplier Name'].str.contains('Z')) | (data_frame['Cost'] > 600.0), :]
+
+data_frame_value_meets_condition.to_csv(output_file, index=False)
+
+# 3 行中的值属于某个集合
+import pandas as pd
+import sys
+
+input_file = 'C:/Users/Administrator/Desktop/Book1.csv'
+output_file = 'C:/Users/Administrator/Desktop/Book8.csv'
+
+data_frame = pd.read_csv(input_file)
+important_dates = ['1/20/14', '1/30/14']
+data_frame_value_in_set = data_frame.loc[data_frame['Purchase Date'].isin(important_dates), :]
+data_frame_value_in_set.to_csv(output_file, index=False)
--- code/读取文件.py
+++ code/读取文件.py
@@ -3,6 +3,7 @@ import sys
 input_file = 'C:/Users/Administrator/Desktop/Book1.csv'
 output_file = 'C:/Users/Administrator/Desktop/Book2.csv'
 
+# 1
 with open(input_file, 'r', newline='') as filereader:
     with open(output_file, 'w', newline='') as filewriter:
         header = filereader.readline()
@@ -15,3 +16,56 @@ with open(input_file, 'r', newline='') as filereader:
             row_list = row.split(',')
             print(row_list)
             filewriter.write(','.join(map(str, row_list)) + '\n')
+
+print('-----------------------')
+
+# 2 使用CSV模块
+import csv, sys
+
+input_file = 'C:/Users/Administrator/Desktop/Book1.csv'
+output_file = 'C:/Users/Administrator/Desktop/Book4.csv'
+
+with open(input_file, 'r', newline='') as csv_in_file:
+    with open(output_file, 'w', newline='') as csv_out_file:
+        filereader = csv.reader(csv_in_file, delimiter=',')
+        filewriter = csv.writer(csv_out_file, delimiter=',')
+        for row_list in filereader:
+            print(row_list)
+            filewriter.writerow(row_list)
+
+# 3 行中值满足某个条件
+import csv, sys
+
+input_file = 'C:/Users/Administrator/Desktop/Book1.csv'
+output_file = 'C:/Users/Administrator/Desktop/Book5.csv'
+
+with open(input_file, 'r', newline='') as csv_in_file:
+    with open(output_file, 'w', newline='') as csv_out_file:
+        filereader = csv.reader(csv_in_file)
+        filewriter = csv.writer(csv_out_file)
+        header = next(filereader)
+        filewriter.writerow(header)
+        for row_list in filereader:
+            supplier = str(row_list[0]).strip()
+            cost = str(row_list[3]).strip("$").replace(',', '')
+            if supplier == 'Supplier Z' or float(cost) > 600.0:
+                filewriter.writerow(row_list)
+
+# print('行中的值属于某个集合')
+import csv, sys
+
+input_file = 'C:/Users/Administrator/Desktop/Book1.csv'
+output_file = 'C:/Users/Administrator/Desktop/Book7.csv'
+
+important_dates = ['1/20/14', '1/30/14']
+
+with open(input_file, 'r', newline='') as csv_in_file:
+    with open(output_file, 'w', newline='') as csv_out_file:
+        filereader = csv.reader(csv_in_file)
+        filewriter = csv.writer(csv_out_file)
+        header = next(filereader)
+        filewriter.writerow(header)
+        for row_list in filereader:
+            a_date = row_list[4]
+            if a_date in important_dates:
+                filewriter.writerow(row_list)
